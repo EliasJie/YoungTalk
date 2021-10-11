b34203671e82d6ea83a90ce2c0b4f19e4c8fba7e
Noroom569 <932544546@qq.com>
2021-10-11 11:05:49 +0800

    此文件是洋桃工作室学习STM32的例程文件

---
+++ 1-2-24-步进电机步数控制程序/User/main.c
@@ -0,0 +1,69 @@
+/*********************************************************************************************
+模板制作：  杜洋工作室/洋桃电子
+程序名：	步进电机步数控制程序
+编写人：	杜洋	
+编写时间：	2018年3月10日
+硬件支持：	STM32F103C8   外部晶振8MHz RCC函数设置主频72MHz　  
+
+修改日志：　　
+1-	
+	
+							
+说明：
+ # 本模板加载了STM32F103内部的RCC时钟设置，并加入了利用滴答定时器的延时函数。
+ # 可根据自己的需要增加或删减。
+
+*********************************************************************************************/
+#include "stm32f10x.h" //STM32头文件
+#include "sys.h"
+#include "delay.h"
+#include "touch_key.h"
+#include "relay.h"
+
+#include "step_motor.h"
+
+int main (void){//主程序
+	RCC_Configuration(); //系统时钟初始化 
+	TOUCH_KEY_Init();//触摸按键初始化
+	RELAY_Init();//继电器初始化
+
+	STEP_MOTOR_Init();//步进电机初始化
+
+	while(1){
+		if(!GPIO_ReadInputDataBit(TOUCH_KEYPORT,TOUCH_KEY_A))STEP_MOTOR_LOOP(0,1,3); // 按圈数右转		
+		else if(!GPIO_ReadInputDataBit(TOUCH_KEYPORT,TOUCH_KEY_B))STEP_MOTOR_LOOP(1,1,3); //按圈数左转		
+		else if(!GPIO_ReadInputDataBit(TOUCH_KEYPORT,TOUCH_KEY_C))STEP_MOTOR_NUM(0,100,3); //按步数右转
+		else if(!GPIO_ReadInputDataBit(TOUCH_KEYPORT,TOUCH_KEY_D))STEP_MOTOR_NUM(1,100,3); //按步数左转
+		else STEP_MOTOR_OFF();//当没有按键时步进电机断电
+	}
+}
+
+/*********************************************************************************************
+ * 杜洋工作室 www.DoYoung.net
+ * 洋桃电子 www.DoYoung.net/YT 
+*********************************************************************************************/
+/*
+
+【变量定义】
+u32     a; //定义32位无符号变量a
+u16     a; //定义16位无符号变量a
+u8     a; //定义8位无符号变量a
+vu32     a; //定义易变的32位无符号变量a
+vu16     a; //定义易变的 16位无符号变量a
+vu8     a; //定义易变的 8位无符号变量a
+uc32     a; //定义只读的32位无符号变量a
+uc16     a; //定义只读 的16位无符号变量a
+uc8     a; //定义只读 的8位无符号变量a
+
+#define ONE  1   //宏定义
+
+delay_us(1); //延时1微秒
+delay_ms(1); //延时1毫秒
+delay_s(1); //延时1秒
+
+GPIO_WriteBit(LEDPORT,LED1,(BitAction)(1)); //LED控制
+
+*/
+
+
+
